eb11797d0a2dd50e3bebec46aef0b046d89ea581
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-20 14:21:51 +0530

    :zap: Adding onClick events to buttons

---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+const registerButton = document.getElementsByClassName('cta-registration')[0];
+const discordButton = document.getElementsByClassName('cta-discord')[0];
+
+registerButton.addEventListener('click', () => {
+    window.open("https://tinyurl.com/OGT-Registration", "_blank");
+});
+
+discordButton.addEventListener('click', () => {
+    window.open("", "_blank");
+});
